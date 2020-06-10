c59be57e59861ab75117726ba80c58c6d160927c
Jim Clark <slimslenderslacks@gmail.com>
2020-06-09 22:58:02 -0700

    add provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+provider "google" {
+  project = "personalsdm-216019"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = "us-central1"
